7c993841a04b6266ed08bdcf0ee26ff212b7ec14
Leander Knosp <leander.knosp@sap.com>
2025-04-30 15:04:27 +0200

    random tiles

--- src/main/java/de/dhbw/catan/controller/BoardController.java
+++ src/main/java/de/dhbw/catan/controller/BoardController.java
@@ -1,5 +1,10 @@
 package de.dhbw.catan.controller;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import javafx.fxml.FXML;
 import javafx.scene.image.Image;
 import javafx.scene.paint.ImagePattern;
@@ -8,48 +13,29 @@ import javafx.scene.shape.Polygon;
 public class BoardController {
 
     @FXML
-    private Polygon hexOcean;
-    @FXML
-    private Polygon hexDesert;
-    @FXML
-    private Polygon hexPastures1;
-    @FXML
-    private Polygon hexPastures2;
-    @FXML
-    private Polygon hexPastures3;
-    @FXML
-    private Polygon hexPastures4;
-    @FXML
-    private Polygon hexForest1;
-    @FXML
-    private Polygon hexForest2;
-    @FXML
-    private Polygon hexForest3;
-    @FXML
-    private Polygon hexForest4;
-    @FXML
-    private Polygon hexMountains1;
-    @FXML
-    private Polygon hexMountains2;
-    @FXML
-    private Polygon hexMountains3;
-    @FXML
-    private Polygon hexHills1;
-    @FXML
-    private Polygon hexHills2;
-    @FXML
-    private Polygon hexHills3;
-    @FXML
-    private Polygon hexFields1;
-    @FXML
-    private Polygon hexFields2;
-    @FXML
-    private Polygon hexFields3;
-    @FXML
-    private Polygon hexFields4;
+    private Polygon hexOcean, hexDesert, hexPastures1, hexPastures2, hexPastures3, hexPastures4, 
+            hexForest1, hexForest2, hexForest3, hexForest4, hexMountains1, hexMountains2, hexMountains3,
+            hexHills1, hexHills2, hexHills3, hexFields1, hexFields2, hexFields3, hexFields4;
 
     @FXML
     public void initialize() {
+        List<Polygon> hexList = List.of(hexPastures1, hexPastures2, hexPastures3, hexPastures4,
+                hexForest1, hexForest2, hexForest3, hexForest4, hexMountains1, hexMountains2, hexMountains3,
+                hexHills1, hexHills2, hexHills3, hexFields1, hexFields2, hexFields3, hexFields4);
+        
+        List<Double> posX = hexList.stream().map(Polygon::getLayoutX).collect(Collectors.toList());
+        List<Double> posY = hexList.stream().map(Polygon::getLayoutY).collect(Collectors.toList());
+
+        List<Polygon> shuffled = new ArrayList<>(hexList);
+        Collections.shuffle(shuffled);
+
+        // Vertauschte Polygone auf die ursprünglichen Positionen setzen
+        for (int i = 0; i < hexList.size(); i++) {
+            Polygon poly = shuffled.get(i);
+            poly.setLayoutX(posX.get(i));
+            poly.setLayoutY(posY.get(i));
+        }
+
         Image imgOcean = new Image(getClass().getResource("/images/ocean.png").toExternalForm());
         Image imgDesert = new Image(getClass().getResource("/images/desert.jpg").toExternalForm());
         Image imgPastures = new Image(getClass().getResource("/images/pastures.jpg").toExternalForm());
@@ -57,6 +43,7 @@ public class BoardController {
         Image imgMountains = new Image(getClass().getResource("/images/mountains.jpg").toExternalForm());
         Image imgHills = new Image(getClass().getResource("/images/hills.jpg").toExternalForm());
         Image imgFields = new Image(getClass().getResource("/images/fields.jpg").toExternalForm());
+
         hexOcean.setFill(new ImagePattern(imgOcean, 0, 0, 0.25, 0.25, true));
         hexDesert.setFill(new ImagePattern(imgDesert));
         hexPastures1.setFill(new ImagePattern(imgPastures));
