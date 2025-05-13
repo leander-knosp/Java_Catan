657a10af53605a1dcaeab921d0a091b4a061d9c5
Leander Knosp <leander.knosp@sap.com>
2025-05-13 10:41:23 +0200

    random numbers

--- src/main/java/de/dhbw/catan/controller/BoardController.java
+++ src/main/java/de/dhbw/catan/controller/BoardController.java
@@ -4,9 +4,9 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
-
 import javafx.fxml.FXML;
 import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Polygon;
 
@@ -18,23 +18,39 @@ public class BoardController {
             hexHills1, hexHills2, hexHills3, hexFields1, hexFields2, hexFields3, hexFields4;
 
     @FXML
+    private AnchorPane circ2, circ3a, circ3b, circ4a, circ4b, circ5a, circ5b, circ6a, circ6b, circ8a, circ8b,
+            circ9a, circ9b, circ10a, circ10b, circ11a, circ11b, circ12;
+
+    @FXML
+    private AnchorPane boardPane;
+    
+    @FXML
     public void initialize() {
+
         List<Polygon> hexList = List.of(hexPastures1, hexPastures2, hexPastures3, hexPastures4,
                 hexForest1, hexForest2, hexForest3, hexForest4, hexMountains1, hexMountains2, hexMountains3,
                 hexHills1, hexHills2, hexHills3, hexFields1, hexFields2, hexFields3, hexFields4);
+
+        List<AnchorPane> circList = List.of(circ2, circ3a, circ3b, circ4a, circ4b, circ5a, circ5b,
+                circ6a, circ6b, circ8a, circ8b, circ9a, circ9b, circ10a, circ10b, circ11a, circ11b, circ12);
         
         List<Double> posX = hexList.stream().map(Polygon::getLayoutX).collect(Collectors.toList());
         List<Double> posY = hexList.stream().map(Polygon::getLayoutY).collect(Collectors.toList());
 
-        List<Polygon> shuffled = new ArrayList<>(hexList);
-        Collections.shuffle(shuffled);
-
+        List<Polygon> shuffledPoly = new ArrayList<>(hexList);
+        List<AnchorPane> shuffledCirc = new ArrayList<>(circList);
+        Collections.shuffle(shuffledCirc);
+        Collections.shuffle(shuffledPoly);
+        
         // Vertauschte Polygone auf die ursprünglichen Positionen setzen
         for (int i = 0; i < hexList.size(); i++) {
-            Polygon poly = shuffled.get(i);
+            Polygon poly = shuffledPoly.get(i);
+            AnchorPane circ = shuffledCirc.get(i);
             poly.setLayoutX(posX.get(i));
             poly.setLayoutY(posY.get(i));
-        }
+            circ.setLayoutX(posX.get(i) - 24);
+            circ.setLayoutY(posY.get(i) - 24);
+        }        
 
         Image imgOcean = new Image(getClass().getResource("/images/ocean.png").toExternalForm());
         Image imgDesert = new Image(getClass().getResource("/images/desert.jpg").toExternalForm());
