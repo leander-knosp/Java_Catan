7dda0f743f85b962782441d483d67b3e89940788
Hannes Klas <hannes.klas@sap.com>
2025-05-20 13:02:13 +0200

    add: player build settlements & roads

--- src/main/java/de/dhbw/catan/controller/PointsController.java
+++ src/main/java/de/dhbw/catan/controller/PointsController.java
@@ -4,6 +4,7 @@ import de.dhbw.catan.model.Board;
 import de.dhbw.catan.model.Edge;
 import de.dhbw.catan.model.Node;
 import de.dhbw.catan.model.Player;
+import de.dhbw.catan.model.ResourceType;
 import de.dhbw.catan.model.BuildingType;
 
 import javafx.fxml.FXML;
@@ -13,6 +14,10 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class PointsController {
 
     private BoardController boardController;
@@ -20,35 +25,51 @@ public class PointsController {
 
     public void setBoardController(BoardController boardController) {
         this.boardController = boardController;
-        this.currentPlayer = new Player("Spieler 1"); // Beispielinitialisierung
+        this.currentPlayer = new Player("Spieler 1");
+        currentPlayer.addResource(ResourceType.BRICK, 10);
+        currentPlayer.addResource(ResourceType.GRAIN, 10);
+        currentPlayer.addResource(ResourceType.LUMBER, 10);
+        currentPlayer.addResource(ResourceType.ORE, 10);
+        currentPlayer.addResource(ResourceType.WOOL, 10);
     }
 
     @FXML
     public void showCornerPoints() {
+        List<Node> nodes = boardController.getBoard().getNodes();
         AnchorPane boardPane = boardController.getBoardPane();
 
         // Vorherige Ecken entfernen
         boardPane.getChildren().removeIf(node -> "corner".equals(node.getUserData()));
 
-        for (Node node : boardController.getBoard().getNodes()) {
-            Circle circle = new Circle(node.getX(), node.getY(), 10);
-            circle.setFill(node.isOccupied() ? Color.RED : Color.WHITE);
-            circle.setStroke(Color.BLACK);
-            circle.setCursor(Cursor.HAND);
-            circle.setUserData("corner");
+        Set<String> uniquePoints = new HashSet<>();
 
-            circle.setOnMouseClicked(event -> {
-                if (!node.isOccupied()) {
-                    node.setOwner(currentPlayer);
-                    node.setBuildingType(BuildingType.SETTLEMENT);
-                    circle.setFill(Color.RED);
-                    System.out.println("Gebäude auf Ecke gesetzt: " + node);
-                } else {
-                    System.out.println("Ecke bereits besetzt: " + node);
-                }
-            });
+        for (Node node : nodes) {
+            String key = String.format("%.3f_%.3f", node.getX(), node.getY());
+            if (uniquePoints.add(key)) {
+                Circle circle = new Circle(node.getX(), node.getY(), 10);
+                circle.setFill(node.isOccupied() ? Color.RED : Color.WHITE);
+                circle.setStroke(Color.BLACK);
+                circle.setCursor(Cursor.HAND);
+                circle.setUserData("corner");
 
-            boardPane.getChildren().add(circle);
+                circle.setOnMouseClicked(event -> {
+                    if (!node.isOccupied()) {
+                        boolean success = currentPlayer.build(BuildingType.SETTLEMENT);
+                        if (success) {
+                            node.setOwner(currentPlayer);
+                            node.setBuildingType(BuildingType.SETTLEMENT);
+                            circle.setFill(Color.RED);
+                            System.out.println("Gebäude auf Ecke gesetzt: " + node);
+                        } else {
+                            System.out.println("Nicht genug Ressourcen für Siedlung.");
+                        }
+                    } else {
+                        System.out.println("Ecke bereits besetzt: " + node);
+                    }
+                });                
+
+                boardPane.getChildren().add(circle);
+            }
         }
     }
 
@@ -59,25 +80,42 @@ public class PointsController {
         // Vorherige Kanten entfernen
         boardPane.getChildren().removeIf(node -> "edge".equals(node.getUserData()));
 
+        Set<String> uniqueEdges = new HashSet<>();
+
         for (Edge edge : boardController.getBoard().getEdges()) {
-            Line line = new Line(edge.getNodeA().getX(), edge.getNodeA().getY(),
-                                 edge.getNodeB().getX(), edge.getNodeB().getY());
-            line.setStroke(edge.isOccupied() ? Color.BLUE : Color.GRAY);
-            line.setStrokeWidth(5);
-            line.setCursor(Cursor.HAND);
-            line.setUserData("edge");
+            String key = edgeKey(edge.getNodeA(), edge.getNodeB());
+            if (uniqueEdges.add(key)) {
+                Line line = new Line(edge.getNodeA().getX(), edge.getNodeA().getY(),
+                                     edge.getNodeB().getX(), edge.getNodeB().getY());
+                line.setStroke(edge.isOccupied() ? Color.BLUE : Color.GRAY);
+                line.setStrokeWidth(5);
+                line.setCursor(Cursor.HAND);
+                line.setUserData("edge");
 
-            line.setOnMouseClicked(event -> {
-                if (!edge.isOccupied()) {
-                    edge.setOwner(currentPlayer);
-                    line.setStroke(Color.BLUE);
-                    System.out.println("Straße auf Kante gesetzt: " + edge);
-                } else {
-                    System.out.println("Kante bereits besetzt: " + edge);
-                }
-            });
+                line.setOnMouseClicked(event -> {
+                    if (!edge.isOccupied()) {
+                        boolean success = currentPlayer.build(BuildingType.ROAD);
+                        if (success) {
+                            edge.setOwner(currentPlayer);
+                            line.setStroke(Color.BLUE);
+                            System.out.println("Straße auf Kante gesetzt: " + edge);
+                        } else {
+                            System.out.println("Nicht genug Ressourcen für Straße.");
+                        }
+                    } else {
+                        System.out.println("Kante bereits besetzt: " + edge);
+                    }
+                });
+                
 
-            boardPane.getChildren().add(line);
+                boardPane.getChildren().add(line);
+            }
         }
+    }
+
+    private String edgeKey(Node a, Node b) {
+        String key1 = String.format("%.3f_%.3f", a.getX(), a.getY());
+        String key2 = String.format("%.3f_%.3f", b.getX(), b.getY());
+        return (key1.compareTo(key2) < 0) ? key1 + "|" + key2 : key2 + "|" + key1;
     }
 }
--- src/main/java/de/dhbw/catan/model/Board.java
+++ src/main/java/de/dhbw/catan/model/Board.java
@@ -22,18 +22,18 @@ public class Board {
     }
 
     private List<Tile> assignTileTypes(List<Polygon> hexes) {
-        List<TileType> TileTypes = new ArrayList<>(List.of(
+        List<TileType> tileTypes = new ArrayList<>(List.of(
                 TileType.PASTURES, TileType.PASTURES, TileType.PASTURES, TileType.PASTURES,
                 TileType.FOREST, TileType.FOREST, TileType.FOREST, TileType.FOREST,
                 TileType.MOUNTAINS, TileType.MOUNTAINS, TileType.MOUNTAINS,
                 TileType.HILLS, TileType.HILLS, TileType.HILLS,
                 TileType.FIELDS, TileType.FIELDS, TileType.FIELDS, TileType.FIELDS
         ));
-        Collections.shuffle(TileTypes);
+        Collections.shuffle(tileTypes);
 
         List<Tile> result = new ArrayList<>();
         for (int i = 0; i < hexes.size(); i++) {
-            result.add(new Tile(hexes.get(i), TileTypes.get(i)));
+            result.add(new Tile(hexes.get(i), tileTypes.get(i)));
         }
         return result;
     }
@@ -43,33 +43,32 @@ public class Board {
         Collections.shuffle(numberTokens);
     }
 
-    private String keyFromCoords(double x, double y) {
-        double precision = 1000.0; // 3 Nachkommastellen
-        long rx = Math.round(x * precision);
-        long ry = Math.round(y * precision);
-        return rx + "_" + ry;
-    }
-    
+    private static final double[][] HEX_OFFSETS = {
+        {-64.95, -37.5}, {-64.95, 37.5}, {0, 75.0},
+        {64.95, 37.5}, {64.95, -37.5}, {0, -75.0}
+    };
+
     private void createNodesAndEdges() {
         Map<String, Node> uniqueNodes = new HashMap<>();
         Set<String> uniqueEdges = new HashSet<>();
         nodes = new ArrayList<>();
         edges = new ArrayList<>();
-    
+
         for (Tile tile : tiles) {
             Polygon hex = tile.getShape();
-            List<Double> points = hex.getPoints();
             double layoutX = hex.getLayoutX();
             double layoutY = hex.getLayoutY();
-    
+            double scaleX = hex.getScaleX();
+            double scaleY = hex.getScaleY();
+
             List<Node> tileNodes = new ArrayList<>();
-    
-            for (int i = 0; i < points.size(); i += 2) {
-                double x = points.get(i) + layoutX;
-                double y = points.get(i + 1) + layoutY;
-    
+
+            for (double[] offset : HEX_OFFSETS) {
+                double x = layoutX + offset[0] * scaleX;
+                double y = layoutY + offset[1] * scaleY;
+
                 String key = keyFromCoords(x, y);
-    
+
                 Node node = uniqueNodes.get(key);
                 if (node == null) {
                     node = new Node(x, y);
@@ -78,11 +77,11 @@ public class Board {
                 }
                 tileNodes.add(node);
             }
-    
+
             for (int i = 0; i < tileNodes.size(); i++) {
                 Node a = tileNodes.get(i);
                 Node b = tileNodes.get((i + 1) % tileNodes.size());
-    
+
                 String edgeKey = edgeKey(a, b);
                 if (!uniqueEdges.contains(edgeKey)) {
                     Edge edge = new Edge(a, b);
@@ -92,12 +91,14 @@ public class Board {
             }
         }
     }
-    
+
+    private String keyFromCoords(double x, double y) {
+        return String.format("%.3f_%.3f", x, y);
+    }
 
     private String edgeKey(Node a, Node b) {
-        // Sortiert nach Position, damit a-b und b-a als gleich erkannt werden
-        String key1 = String.format("%.3f_%.3f", a.getX(), a.getY());
-        String key2 = String.format("%.3f_%.3f", b.getX(), b.getY());
+        String key1 = keyFromCoords(a.getX(), a.getY());
+        String key2 = keyFromCoords(b.getX(), b.getY());
         return (key1.compareTo(key2) < 0) ? key1 + "|" + key2 : key2 + "|" + key1;
     }
 }
